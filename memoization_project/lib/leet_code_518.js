c95bf45c2fb2d7974a885dc5a4873c4cc7da6969
Michelle Kim <x1michelleskim@gmail.com>
2019-09-25 07:49:26 -0700

    Solve coin change 2

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -33,7 +33,23 @@
 // can we assume the coins will always be sorted so that the last value is the highest coin value always;
 // use the highest coin and then once we exhaust it, we remove it?
 var change = function(amount, coins, memo = {}) {
-    if (amount === 0) return 0;
+    let key = amount + '-' + coins;
+    if (key in memo) return memo[key]
+    if (amount === 0) return 1;
 
+    let currentCoin = coins[coins.length - 1];
+
+    let total = 0;
+
+    for (let qty = 0; qty * currentCoin <= amount; qty++) {
+        total += change(amount - qty * currentCoin, coins.slice(0, -1), memo)
+    }
+
+    memo[key] = total
+    return memo[key];
 
 };
+
+change(5,[1, 2, 5]);
+change(3, [2]);
+change(10, [10]);
